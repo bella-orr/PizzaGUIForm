a500b69f1847fd6f105596f237060a6c19df2f98
bella-orr <143103302+bella-orr@users.noreply.github.com>
2023-10-18 12:37:06 -0400

    Panels Created

--- src/PizzaGUIFrame.java
+++ src/PizzaGUIFrame.java
@@ -1,8 +1,13 @@
 //imports
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
 import java.util.*;
+
+import static java.awt.Color.black;
 
 public class PizzaGUIFrame extends JFrame
 {
@@ -13,13 +18,30 @@ public class PizzaGUIFrame extends JFrame
     JPanel sizePnl;
     JPanel crustPnl;
     JPanel toppingsPnl;
-    JPanel receiptPnl;
+    JScrollPane receiptPnl;
     JPanel buttonPnl;
+    JPanel bottomPnl;
 
     //JButtons
     JButton orderbtn;
     JButton clearbtn;
     JButton quitbtn;
+
+    //JRadioButtons
+    JRadioButton thinCrust;
+    JRadioButton regularCrust;
+    JRadioButton deepDishCrust;
+
+    //JComboBox
+    JComboBox sizeSelect;
+
+    //JCheckBoxes
+    JCheckBox pepBox;
+    JCheckBox oliveBox;
+    JCheckBox mushroomBox;
+    JCheckBox pineappleBox;
+    JCheckBox hamBox;
+    JCheckBox pepperBox;
 
     //JScroller
     JScrollPane scroller;
@@ -27,13 +49,22 @@ public class PizzaGUIFrame extends JFrame
     //JTextAres
     JTextArea receipt;
 
+    //for JoptionPane
+    int reply;
+
+    //ints for selected
+    int selSize;
+    int amountToppings;
+    int selTopping = 0;
+    
+
     public PizzaGUIFrame()
     {
         mainPnl = new JPanel();
-        mainPnl.setLayout(new BorderLayout());
+        mainPnl.setLayout(new GridLayout(5,1));
 
         createSizePnl();
-        mainPnl.add(sizePnl, BorderLayout.NORTH);
+        mainPnl.add(sizePnl);
 
         createCrustPnl();
         mainPnl.add(crustPnl);
@@ -45,32 +76,89 @@ public class PizzaGUIFrame extends JFrame
         mainPnl.add(receiptPnl);
 
         createButtonPnl();
-        mainPnl.add(buttonPnl, BorderLayout.SOUTH);
+        mainPnl.add(buttonPnl);
+
 
         add(mainPnl);
         setTitle("Pizza GUI Order");
-        setSize(700, 700);
+        setSize(900, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     private void createSizePnl()
     {
+        sizePnl = new JPanel();
 
+        //border created for SizePnl
+        sizePnl.setBorder(new TitledBorder((new LineBorder(black, 4 )), "Pick A Size!"));
+
+        String sizes[] = {"Small","Medium", "Large", "Super"};
+        sizeSelect = new JComboBox(sizes);
+
+        sizePnl.add(sizeSelect);
     }
 
     private void createCrustPnl()
     {
+        crustPnl = new JPanel();
+
+        //border created for CrustPnl
+        crustPnl.setBorder(new TitledBorder((new LineBorder(black, 4 )), "Pick A Crust!"));
+
+        //initialize the buttons
+        thinCrust = new JRadioButton("Thin");
+        regularCrust = new JRadioButton("Regular");
+        deepDishCrust = new JRadioButton("Deep Dish");
+
+        crustPnl.add(thinCrust);
+        crustPnl.add(regularCrust);
+        crustPnl.add(deepDishCrust);
 
     }
 
     private void createToppingsPnl()
     {
+        toppingsPnl = new JPanel();
+
+        toppingsPnl.setBorder(new TitledBorder((new LineBorder(black, 4 )), "Pick Some Toppings!"));
+
+        toppingsPnl.setLayout(new FlowLayout());
+
+        pepBox = new JCheckBox("Pepperoni");
+        oliveBox = new JCheckBox("Olives");
+        mushroomBox = new JCheckBox("Mushrooms");
+        pineappleBox = new JCheckBox("Pineapple");
+        hamBox = new JCheckBox("Ham");
+        pepperBox = new JCheckBox("Peppers");
+
+        toppingsPnl.add(pepBox);
+        toppingsPnl.add(oliveBox);
+        toppingsPnl.add(mushroomBox);
+        toppingsPnl.add(pineappleBox);
+        toppingsPnl.add(hamBox);
+        toppingsPnl.add(pepperBox);
+
+        getTotalToppings();
 
     }
 
+    private void getTotalToppings() {
+    }
+
+
     private void createReceiptPnl()
     {
+        receiptPnl = new JScrollPane();
+
+        receipt = new JTextArea(40,50);
+        receipt.setEditable(false);
+
+        receiptPnl.setBorder(new TitledBorder((new LineBorder(black, 4 )), "Receipt:"));
+
+
+        scroller = new JScrollPane(receipt);
+        receiptPnl.add(scroller);
 
     }
 
@@ -80,11 +168,37 @@ public class PizzaGUIFrame extends JFrame
         buttonPnl.setLayout(new GridLayout(1,3));
 
         orderbtn = new JButton("Order Now!");
+        orderbtn.addActionListener((ActionEvent ae) -> calcTotal());
 
         clearbtn = new JButton("Clear Order");
 
         quitbtn = new JButton("Quit");
+        quitbtn.addActionListener((ActionEvent ae) -> quitProgram());
 
+        buttonPnl.add(orderbtn);
+
+        buttonPnl.add(clearbtn);
+
+        buttonPnl.add(quitbtn);
+    }
+
+    private void calcTotal()
+    {
 
     }
+
+    private void quitProgram()
+    {
+        reply=JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?");
+
+        if (reply == JOptionPane.YES_OPTION) //resets board
+        {
+            System.exit(0);
+        }
+
+    }
+
+
+
+
 }
